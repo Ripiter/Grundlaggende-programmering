7b98c86c7d1790b15dc01c457fc6900482c529ad
Peter <ripiter15@wp.pl>
2019-01-25 09:51:59 +0100

    Done with porto

--- Word H1/Porto.cs
+++ Word H1/Porto.cs
@@ -8,9 +8,93 @@ namespace Word_H1
 {
     class Porto
     {
-        public static void Main(string[] args)
-        {
 
-        }
+        //public static void Main(string[] args)
+        //{
+        //    double Size;
+        //    double Weight;
+        //    Console.Write("What Country: ");
+        //    string userCountry = Convert.ToString(Console.ReadLine());
+
+        //    Console.Write("Input weight: ");
+        //    Weight = double.Parse(Console.ReadLine());
+        //    Console.Write("Input height: ");
+        //    double Height = double.Parse(Console.ReadLine());
+        //    Console.Write("Input length: ");
+        //    double Length = double.Parse(Console.ReadLine());
+        //    Console.Write("Input Width: ");
+        //    double Width = double.Parse(Console.ReadLine());
+
+        //    Size = Length + Width + Height;
+        //    GetPrice(userCountry, Weight, Size);
+        //    Console.ReadKey();
+
+        //}
+        //public static double GetPrice(string country, double weight, double size)
+        //{
+        //    double priceDanmark = 0;
+        //    double priceOutside = 0;
+        //    if (country.ToLower() == "danmark")
+        //    {
+        //        if (size > 90)
+        //        {
+        //            Console.WriteLine("It's a package");
+        //            if (weight < 50)
+        //            {
+        //                priceDanmark = 60;
+        //                Console.WriteLine("Price: " + priceDanmark + "kr");
+        //                return priceDanmark;
+        //            }
+        //            else
+        //            {
+        //                priceDanmark = 100;
+        //                Console.WriteLine("Price: " + priceDanmark + "kr");
+        //                return priceDanmark;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("It's a letter");
+        //            if (weight < 50)
+        //            {
+        //                priceDanmark = 10;
+        //                Console.WriteLine("Price: " + priceDanmark + "kr");
+        //                return priceDanmark;
+        //            }
+                    
+        //        }
+        //        return priceDanmark;
+        //    }
+        //    else
+        //    {
+        //        if (size > 90)
+        //        {
+        //            Console.WriteLine("It's a package");
+        //            if (weight < 50)
+        //            {
+        //                priceOutside = 90;
+        //                Console.WriteLine("Price: " + priceOutside + "kr");
+        //                return priceOutside;
+        //            }
+        //            else
+        //            {
+        //                priceOutside = 180;
+        //                Console.WriteLine("Price: " + priceOutside + "kr");
+        //                return priceOutside;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("It's a letter");
+        //            if (weight < 50)
+        //            {
+        //                priceOutside = 30;
+        //                Console.WriteLine("Price: " + priceOutside + "kr");
+        //                return priceOutside;
+        //            }
+        //        }
+        //        return priceOutside;
+        //    }
+        //}
     }
 }
