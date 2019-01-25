6977be9cd00161aba1b3b51014b69be569d11836
Peter <ripiter15@wp.pl>
2019-01-25 10:16:46 +0100

    Morse code done

---
+++ Word H1/MorseKoden.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_H1
+{
+    class MorseKoden
+    {
+        //public static void Main(string[] args)
+        //{
+        //    while(1 == 1)
+        //    {
+        //        Console.Write("Insert tekst you want to translate: ");
+        //        string text = Console.ReadLine();
+        //        char[] chararay = text.ToCharArray();
+                
+
+
+        //        for (int i = 0; i < chararay.Length; i++)
+        //        {
+        //            switch (chararay[i])
+        //            {
+        //                case 'a':
+        //                    Console.WriteLine(".-");
+        //                    break;
+        //                case 'b':
+        //                    Console.WriteLine("-...");
+        //                    break;
+        //                case 'c':
+        //                    Console.WriteLine("-.-.");
+        //                    break;
+        //                case 'd':
+        //                    Console.WriteLine("-..");
+        //                    break;
+        //                case 'e':
+        //                    Console.WriteLine(".");
+        //                    break;
+        //                case 'f':
+        //                    Console.WriteLine("..-");
+        //                    break;
+        //                case 'g':
+        //                    Console.WriteLine("--.");
+        //                    break;
+        //                case 'h':
+        //                    Console.WriteLine("....");
+        //                    break;
+        //                case 'i':
+        //                    Console.WriteLine("..");
+        //                    break;
+        //                case 'j':
+        //                    Console.WriteLine(".---");
+        //                    break;
+        //                case 'k':
+        //                    Console.WriteLine("-.-");
+        //                    break;
+        //                case 'l':
+        //                    Console.WriteLine(".-..");
+        //                    break;
+        //                case 'm':
+        //                    Console.WriteLine("--");
+        //                    break;
+        //                case 'n':
+        //                    Console.WriteLine("-.");
+        //                    break;
+        //                case 'o':
+        //                    Console.WriteLine("---");
+        //                    break;
+        //                case 'p':
+        //                    Console.WriteLine(".--.");
+        //                    break;
+        //                case 'q':
+        //                    Console.WriteLine("--.-");
+        //                    break;
+        //                case 'r':
+        //                    Console.WriteLine(".-.");
+        //                    break;
+        //                case 's':
+        //                    Console.WriteLine("...");
+        //                    break;
+        //                case 't':
+        //                    Console.WriteLine("-");
+        //                    break;
+        //                case 'u':
+        //                    Console.WriteLine("..-");
+        //                    break;
+        //                case 'v':
+        //                    Console.WriteLine("...-");
+        //                    break;
+        //                case 'w':
+        //                    Console.WriteLine(".--");
+        //                    break;
+        //                case 'x':
+        //                    Console.WriteLine(" -..- ");
+        //                    break;
+        //                case 'y':
+        //                    Console.WriteLine(" -..- ");
+        //                    break;
+        //                case 'z':
+        //                    Console.WriteLine("--..");
+        //                    break;
+        //                case 'æ':
+        //                    Console.WriteLine(".-.-");
+        //                    break;
+        //                case 'ø':
+        //                    Console.WriteLine("---.");
+        //                    break;
+        //                case 'å':
+        //                    Console.WriteLine(".--.-");
+        //                    break;
+
+        //                default:
+        //                    Console.WriteLine("");
+        //                    break;
+        //            }
+
+        //            Console.ReadLine();
+        //        }
+
+        //    }
+
+        //}
+    }
+}
