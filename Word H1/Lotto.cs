98ae5d51dd789479523481c424843714b8df1518
Peter <ripiter15@wp.pl>
2019-01-29 13:03:41 +0100

    Fixed + switch

--- Word H1/Lotto.cs
+++ Word H1/Lotto.cs
@@ -8,35 +8,63 @@ namespace Word_H1
 {
     class Lotto
     {
-        public static void Main(string[] args)
-        {
-            int[] lottoTal = new int[7];
-            int[] userCupon = new int[7] { 1, 2, 3, 4, 5, 7, 6, };
-            int[] winningCupons;
-            int win = 0;
+        //public static void Main(string[] args)
+        //{
+        //    int[] lottoTal = new int[7];
+        //    int[] userCupon = new int[7] { 1, 2, 3, 4, 5, 7, 6, };
+        //    int[] winningCupons;
+        //    int win = 0;
 
-            Random randNum = new Random();
+        //    Random randNum = new Random();
 
-            for (int i = 0; i < userCupon.Length; i++)
-            {
-                lottoTal[i] = randNum.Next(1, 20);              //Give lottotal a number between 1 and 20
+        //    for (int i = 0; i < userCupon.Length; i++)
+        //    {
+        //        lottoTal[i] = randNum.Next(1, 20);              //Give lottotal a number between 1 and 20
 
-                Console.WriteLine("lotto " + lottoTal[i]);      //Prints lottotal to check the numbers
-                Console.WriteLine("user " + userCupon[i]);      //Prints user number to compare to lottotal
-
-                
-                winningCupons = lottoTal;
-
-                for (int j = 0; j < userCupon.Length; j++)
-                {
-                    if (winningCupons[i] == userCupon[j]) //REMEMBER to set i and j in the right place
-                        win++;
-                }
-            }
+        //        Console.WriteLine("lotto " + lottoTal[i]);      //Prints lottotal to check the numbers
+        //        Console.WriteLine("user " + userCupon[i]);      //Prints user number to compare to lottotal
 
 
-            Console.WriteLine("You won: " + win + " time(s)");
-            Console.ReadLine();
-        }
+        //        winningCupons = lottoTal;
+
+        //        for (int j = 0; j < userCupon.Length; j++)
+        //        {
+        //            if (winningCupons[i] == userCupon[j]) //REMEMBER to set i and j in the right place
+        //                win++;
+        //        }
+        //    }
+
+
+        //    Console.WriteLine("You won: " + win + " time(s)");
+
+        //    switch (win) {
+        //        case 1:
+        //            Console.WriteLine("You didnt win anything");
+        //            break;
+        //        case 2:
+        //            Console.WriteLine("Congratulation you have won 10kr");
+        //            break;
+        //        case 3:
+        //            Console.WriteLine("Congratulation you have won 25kr");
+        //            break;
+        //        case 4:
+        //            Console.WriteLine("Congratulation you have won 50kr");
+        //            break;
+        //        case 5:
+        //            Console.WriteLine("Congratulation you have won 100kr");
+        //            break;
+        //        case 6:
+        //            Console.WriteLine("Congratulation you have won 250kr");
+        //            break;
+        //        case 7:
+        //            Console.WriteLine("Congratulation you have won 1.000.000kr");
+        //            break;
+        //        default:
+        //            Console.WriteLine("You Won Nothing!");
+        //            break;
+
+        //    }
+        //    Console.ReadLine();
+        //}
     }
 }
