8ce94580ff4c3f4dfa137942f7a9a017b0dd3c47
Peter <ripiter15@wp.pl>
2019-01-29 10:27:57 +0100

    FIXED IT

--- Word H1/Lotto.cs
+++ Word H1/Lotto.cs
@@ -12,8 +12,9 @@ namespace Word_H1
         {
             int[] lottoTal = new int[7];
             int[] userCupon = new int[7] { 1, 2, 3, 4, 5, 7, 6, };
-            int win = 0;
             int[] winningCupons;
+            int win = 0;
+
             Random randNum = new Random();
 
             for (int i = 0; i < userCupon.Length; i++)
@@ -23,19 +24,18 @@ namespace Word_H1
                 Console.WriteLine("lotto " + lottoTal[i]);      //Prints lottotal to check the numbers
                 Console.WriteLine("user " + userCupon[i]);      //Prints user number to compare to lottotal
 
+                
                 winningCupons = lottoTal;
 
-
-
-                //Sets all lottotal into a winningcupon
-                if (winningCupons == userCupon)
+                for (int j = 0; j < userCupon.Length; j++)
                 {
-                    win++;
+                    if (winningCupons[i] == userCupon[j]) //REMEMBER to set i and j in the right place
+                        win++;
                 }
-                Console.WriteLine("SHOW ME ARRAY" + winningCupons[6]);  //tests to show what is in X position
-                Console.WriteLine("You won: " + win + " time(s)");      //prints how many times you won
             }
 
+
+            Console.WriteLine("You won: " + win + " time(s)");
             Console.ReadLine();
         }
     }
