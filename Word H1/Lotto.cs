23afec7cb605e42d56f25359b4f1754bd55a05ac
Peter <ripiter15@wp.pl>
2019-01-29 09:27:35 +0100

    No harcoding

--- Word H1/Lotto.cs
+++ Word H1/Lotto.cs
@@ -8,33 +8,35 @@ namespace Word_H1
 {
     class Lotto
     {
-        public static void Main(string[] args)
-        {
-            int[] lottoTal = new int[7];
-            int[] userCupon = new int[7] {1, 2, 3, 4, 5, 7, 6,};
-            int win = 0;
+        //public static void Main(string[] args)
+        //{
+        //    int[] lottoTal = new int[7];
+        //    int[] userCupon = new int[7] { 1, 2, 3, 4, 5, 7, 6, };
+        //    int win = 0;
+        //    int[] winningCupons;
+        //    Random randNum = new Random();
 
-            // this declares an integer array with 5 elements
-            // and initializes all of them to their default value
-            // which is zero
+        //    for (int i = 0; i < userCupon.Length; i++)
+        //    {
+        //        lottoTal[i] = randNum.Next(1, 20);              //Give lottotal a number between 1 and 20
 
-            Random randNum = new Random();
+        //        Console.WriteLine("lotto " + lottoTal[i]);      //Prints lottotal to check the numbers
+        //        Console.WriteLine("user " + userCupon[i]);      //Prints user number to compare to lottotal
 
-            for(int i = 0; i < lottoTal.Length; i++)
-            {
-                lottoTal[e] = randNum.Next(0, 20);
+        //        winningCupons = lottoTal;    
+                
+                             
+                
+        //        //Sets all lottotal into a winningcupon
+        //        if (winningCupons == userCupon)
+        //        {
+        //            win++;
+        //        }
+        //        Console.WriteLine("SHOW ME ARRAY" + winningCupons[6]);  //tests to show what is in X position
+        //        Console.WriteLine("You won: " + win + " time(s)");      //prints how many times you won
+        //    }
 
-                Console.WriteLine("lotto " + lottoTal[i]);
-                Console.WriteLine("user " + userCupon[i]);
-
-                    if (lottoTal == userCupon)      //!!!Problem!!!
-                    {                               //problem is that it checks like a switch and not full
-                        win = win +1;               //so it goes lottotal is 2 usercupon 1
-                    }                               //it goes again, lottotal 1 and usercuper 2
-                                                    //and it doesnt count as a win
-            }
-            Console.WriteLine("You won: " + win + " time(s)");
-            Console.ReadLine();
-        }
+        //    Console.ReadLine();
+        //}
     }
 }
--- Word H1/liste.cs
+++ Word H1/liste.cs
@@ -8,38 +8,60 @@ namespace Word_H1
 {
     class liste
     {
-        //public static void Main(string[] args)
-        //{
-        //     // listeB.RemoveAll(item => item != 3); i know you can make it with removeall but i cant find the way to do it
+        
+        public static void Main(string[] args)
+        {
+            // listeB.RemoveAll(item => item != 3); i know you can make it with removeall but i cant find the way to do it
+            List<int> listeB = new List<int>() { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+
+
+            //listeB.Remove(6);
+            //listeB.Remove(12);
+            //listeB.Remove(18);
+            for (int i = 0; i < listeB.Count; i++)
+            {
+                if (listeB[i] % 3 == 0) //removes all the numbers that can be divided by 3
+                    listeB.RemoveAt(i);
+            }
+
+
+
+            listeB.Insert(2, 17);
+
+
+            foreach (int i in listeB)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Numbers in the list");
+            Console.WriteLine(listeB.Count);
+
+            testing();
+            Print();
+
+            Console.WriteLine("Reverse");
+
+            listeB.Reverse();
+            foreach (int krak in listeB)
+            {
+                Console.WriteLine(krak);
+            }
+            Print();
+            Console.ReadLine();
+                void Print()
+                {
+                    Console.WriteLine("hello world");
+                }
+
+        }
+        static void testing()
+        {
+            Console.WriteLine("Hello There");
+            Console.WriteLine("General Kenobi");
             
-        //    List<int> listeB = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-
-        //    listeB.Remove(3);
-        //    listeB.Remove(6);
-        //    listeB.Remove(9);
-        //    listeB.Remove(12);
-        //    listeB.Remove(18);
-
-        //    listeB.Insert(2, 17);
-
-
-        //    foreach (int i in listeB)
-        //    {
-        //        Console.WriteLine(i);
-        //    }
-        //    Console.WriteLine("Numbers in the list");
-        //        Console.WriteLine(listeB.Count);
-
-        //    Console.WriteLine("Reverse");
-
-        //    listeB.Reverse();
-        //    foreach (int krak in listeB)
-        //    {
-        //        Console.WriteLine(krak);
-        //    }
-
-        //    Console.ReadLine();
-        //}
+            
+        }
 
     }
 }
