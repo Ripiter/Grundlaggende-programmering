bed59a8ca8a14bced3f824d9f34cd18082dee8fe
Peter <ripiter15@wp.pl>
2019-02-17 10:35:49 +0100

    Methods(not ignored)

---
+++ Word H1/MetoderTutoriaal.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_H1
+{
+    class MetoderTutoriaal
+    {
+        //static void Main(string[] args)
+        //{
+        //    string tekst = "hej"; //send string to print method 
+        //    int num = 17;         //send int to print method
+
+        //    Print();              //Prints the print method with any variables
+
+        //    Print(tekst);         //prints method tekst
+        //    Print(tekst + " " + num);    //prints method tekst with the number
+            
+
+        //    Console.Write("number: ");
+        //    int numberFirst = int.Parse(Console.ReadLine());
+        //    Console.Write("number 2: ");
+        //    int numberBy = int.Parse(Console.ReadLine());
+        //    Console.Write("square root number: ");
+        //    int numberSqr = int.Parse(Console.ReadLine());
+
+
+        //    int plus = MathPlus(numberFirst, numberBy); //User input gets used by MathPlus
+        //    int minus = MathMinus(numberFirst, numberBy); //User input gets used by MathMinus
+        //    double sqr = MathSqr(numberSqr);                 //User input gets used by MathSqr
+        //    double sum = Power(numberFirst, numberBy); //User input gets used by power method
+
+
+
+        //    Console.WriteLine("plus " + plus); //Print the end result of the MathPlus method
+        //    Console.WriteLine("minus " + minus); //Print the end result of the MathMinus method
+        //    Console.WriteLine("sqr: " + sqr);   //Print the end result of the MathSqr method
+        //    Console.WriteLine("power: " + sum);  //Prints the end result of the Power method
+
+        //    Console.ReadLine();
+        //}
+
+
+        ///// <summary>
+        ///// Print stuff
+        ///// </summary>
+
+        //public static void Print()
+        //{
+        //    Console.WriteLine("Hello World");
+        //}
+
+        //public static void Print(string txt) //Name of the method can be the same as long as it doesnt have
+        //{                                    //the same value 
+        //    Console.WriteLine(txt);
+        //}
+
+        //public static void Print(string txt, int number) //One is string, and other is string AND number
+        //{
+        //    Console.WriteLine(txt, number);
+        //}
+
+
+        ///// <summary>
+        ///// Do some math
+        ///// </summary>
+
+
+        //public static int MathPlus(int a, int b)
+        //{
+        //    int resultPlus = a + b;
+
+        //    return resultPlus;
+        //}
+
+        //public static int MathMinus(int a, int b)
+        //{
+        //    int resultMinus = a - b;
+
+        //    return resultMinus;
+        //}
+        //public static double MathSqr(int a)
+        //{
+        //        double resultSqr = Math.Sqrt(a);
+
+        //        return resultSqr;  
+        //}
+        //public static double Power(int number,int po)
+        //{
+        //    double total = Math.Pow(number, po); //first number to power(^) by userinput second number 
+
+        //    return total;
+        //} 
+        
+    }
+}
---
+++ Word H1/Metoderr.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_H1
+{
+    class Metoderr
+    {
+        //public static void Main(string[] args)
+        //{
+        //    Console.Write("Gimme your first number: ");
+        //    int userInputA = int.Parse(Console.ReadLine());
+        //    Console.Write("Gimme your second number: ");
+        //    int userInputB = int.Parse(Console.ReadLine());
+
+
+        //    void DoMath()
+        //    {
+        //        int answear = userInputA / userInputB;
+        //        Console.WriteLine("Answear is: " + answear);
+        //    }
+
+
+        //    DoMath();
+
+        //    Console.ReadLine();
+        //}
+    }
+}
